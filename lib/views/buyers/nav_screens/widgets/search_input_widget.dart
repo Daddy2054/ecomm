24b58dc2d8ac838c4b0c741569dead59f00b25aa
Jean <acalto2@gmail.com>
2023-12-14 23:11:55 +0100

    14. Add Packages

--- lib/views/buyers/nav_screens/widgets/search_input_widget.dart
+++ lib/views/buyers/nav_screens/widgets/search_input_widget.dart
@@ -13,16 +13,17 @@ class SearchInputWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         child: const TextField(
           decoration: InputDecoration(
-              fillColor: Colors.white,
-              filled: true,
-              hintText: 'Search For Products',
-              border: OutlineInputBorder(
-                borderSide: BorderSide.none,
-              ),
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(14.0),
-                child: Icon(Icons.search),
-              )),
+            fillColor: Colors.white,
+            filled: true,
+            hintText: 'Search For Products',
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            prefixIcon: Padding(
+              padding: EdgeInsets.all(14.0),
+              child: Icon(Icons.search),
+            ),
+          ),
         ),
       ),
     );
